246a26206f26058e714cf5973383f24b280b863f
Kevin Pham <kevintp5@uci.edu>
2026-06-12 09:53:52 -0700

    feat: create POM for homepage
    Successful login redirects from login page to homepage.

---
+++ playwright/pages/HomePage.js
@@ -0,0 +1,8 @@
+class HomePage {
+    constructor (page) {
+        this.page = page;
+        this.logoutButton = page.getByRole('button', { name: 'Logout' });
+    }
+}
+
+export default HomePage;
--- playwright/tests/login.spec.js
+++ playwright/tests/login.spec.js
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/LoginPage';
+import HomePage from '../pages/HomePage';
 
 test.describe('customer login', () => {
     let loginPage;
@@ -9,21 +10,22 @@ test.describe('customer login', () => {
         await loginPage.goto();
     });
 
-    test('page loaded', async ({ page }) => {
+    test('page loaded', async () => {
         await expect(loginPage.signInHeader).toBeVisible();
         await expect(loginPage.emailInput).toBeVisible();
         await expect(loginPage.passwordInput).toBeVisible();
         await expect(loginPage.loginButton).toBeVisible();
     });
 
-    test('incorrect password', async ({ page }) => {
+    test('incorrect password', async () => {
         await loginPage.login("a@email.com", "a1");
         await expect(loginPage.page.getByText('Incorrect username or password')).toBeVisible();
     });
 
     test('successful', async ({ page }) => {
+        let homePage = new HomePage(page);
+
         await loginPage.login("a@email.com", "a2");
-        await expect(page).toHaveURL(/index\.html$/);
-        await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+        await expect(homePage.logoutButton).toBeVisible();
     });
 });
