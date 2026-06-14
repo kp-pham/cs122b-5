a273ba00cf1795c02cef8c3e269873b389e6c936
Kevin Pham <kevintp5@uci.edu>
2026-06-14 16:51:28 -0700

    refactor: test login and logout lifecycle
    Perform end-to-end testing with user journeys

--- playwright/tests/homepage.spec.js
+++ playwright/tests/homepage.spec.js
@@ -1,13 +1,1 @@
 import { test, expect } from './fixtures/base.fixture';
-
-test.describe("homepage", () => {
-    test('redirects to login page upon logout', async ({ loginPage, homePage }) => {
-        await homePage.goto();
-        await expect(homePage.page).toHaveURL('https://localhost:8443/cs122b_war/index.html');
-        await homePage.logout();
-        await expect(loginPage.signInHeader()).toBeVisible();
-        await expect(loginPage.emailInput()).toBeVisible();
-        await expect(loginPage.passwordInput()).toBeVisible();
-        await expect(loginPage.loginButton()).toBeVisible();
-    });
-});
--- playwright/tests/login.spec.js
+++ playwright/tests/login.spec.js
@@ -3,23 +3,28 @@ import { test, expect } from './fixtures/base.fixture';
 test.use({ storageState: { cookies: [], origins: [] } });
 
 test.describe('customer login', () => {
-    test('page loaded', async ({ loginPage }) => {
+    test('error message for incorrect message', async ({ loginPage }) => {
         await loginPage.goto();
         await expect(loginPage.signInHeader()).toBeVisible();
         await expect(loginPage.emailInput()).toBeVisible();
         await expect(loginPage.passwordInput()).toBeVisible();
         await expect(loginPage.loginButton()).toBeVisible();
-    });
 
-    test('incorrect password', async ({ loginPage }) => {
-        await loginPage.goto();
         await loginPage.login("a@email.com", "a1");
         await expect(loginPage.page.getByText('Incorrect username or password')).toBeVisible();
     });
 
-    test('successful', async ({ loginPage, homePage }) => {
+    test('login and loout lifecycle', async ({ loginPage, homePage }) => {
+        // Redirect to homepage when correct email and password provided
         await loginPage.goto();
         await loginPage.login("a@email.com", "a2");
         await expect(homePage.logoutButton()).toBeVisible();
+
+        // Redirect to login page when logging out
+        await homePage.logout();
+        await expect(loginPage.signInHeader()).toBeVisible();
+        await expect(loginPage.emailInput()).toBeVisible();
+        await expect(loginPage.passwordInput()).toBeVisible();
+        await expect(loginPage.loginButton()).toBeVisible();
     });
 });
